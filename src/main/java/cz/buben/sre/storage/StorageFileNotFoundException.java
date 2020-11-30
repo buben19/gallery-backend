f96053b52749c1f9656c1cbd53bf896d0f62fd28
Ivo Slanina <ivo.slanina@gmail.com>
2020-11-30 17:54:21 +0100

    Added basic file storage classes

---
+++ src/main/java/cz/buben/sre/storage/StorageFileNotFoundException.java
@@ -0,0 +1,20 @@
+package cz.buben.sre.storage;
+
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException() {
+    }
+
+    public StorageFileNotFoundException(String s) {
+        super(s);
+    }
+
+    public StorageFileNotFoundException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public StorageFileNotFoundException(Throwable throwable) {
+        super(throwable);
+    }
+}
