e3bb5564bee5bad443189a78df6dbca80e9f2d15
Ivo Slanina <ivo.slanina@gmail.com>
2020-12-03 14:47:15 +0100

    Added storage path generator

---
+++ src/main/java/cz/buben/sre/storage/StoragePathGenerator.java
@@ -0,0 +1,44 @@
+package cz.buben.sre.storage;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+@Component
+@AllArgsConstructor
+public class StoragePathGenerator {
+
+    private final Supplier<UUID> uuidSupplier;
+
+    /**
+     * Create path for media file.
+     *
+     * @param original Original file.
+     *
+     * @return Generated path within archive root directory.
+     */
+    @Nonnull
+    public Path getStoragePath(@Nonnull Path original) {
+        Optional<String> extensionOptional = this.getFileExtension(original);
+        StringBuilder builder = new StringBuilder();
+        builder.append(this.uuidSupplier.get().toString());
+        extensionOptional.ifPresent(s -> builder.append(".").append(s));
+        return Paths.get(builder.toString());
+    }
+
+    private Optional<String> getFileExtension(Path path) {
+        return this.getFileExtension(path.getFileName().toString());
+    }
+
+    private Optional<String> getFileExtension(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+    }
+}
---
+++ src/test/groovy/cz/buben/sre/storage/StoragePathGeneratorSpecification.groovy
@@ -0,0 +1,34 @@
+package cz.buben.sre.storage
+
+import spock.lang.Specification
+
+import java.nio.file.Paths
+import java.util.function.Supplier
+
+class StoragePathGeneratorSpecification extends Specification {
+
+    Supplier<UUID> uuidSupplier = Mock()
+    StoragePathGenerator generator = new StoragePathGenerator(uuidSupplier)
+
+    def "generate for file without extension"() {
+        when:
+        def path = generator.getStoragePath(Paths.get("a/b/c/foo"))
+
+        then:
+        1 * uuidSupplier.get() >> new UUID(1, 2)
+
+        and:
+        path == Paths.get('00000000-0000-0001-0000-000000000002')
+    }
+
+    def "generate for file with extension"() {
+        when:
+        def path = generator.getStoragePath(Paths.get("a/b/c/foo.jpg"))
+
+        then:
+        1 * uuidSupplier.get() >> new UUID(1, 2)
+
+        and:
+        path == Paths.get('00000000-0000-0001-0000-000000000002.jpg')
+    }
+}
