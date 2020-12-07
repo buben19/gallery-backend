6900085ec6b7db697cc7839600beb26ec821d328
Ivo Slanina <ivo.slanina@gmail.com>
2020-12-07 15:17:00 +0100

    Removed dist script

--- frontend/dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-app_name=$(cat package.json | jq '.name' -r)
-app_version=$(cat package.json | jq '.version' -r)
-archive="${app_name}-${app_version}.tar.gz"
-ng.sh build --environment 'production-nec-test'
-tar -czf "$archive" "dist" --transform='s/\dist/\/static/'
-cp "$archive" "/var/www/repository/adsb-frontend"
