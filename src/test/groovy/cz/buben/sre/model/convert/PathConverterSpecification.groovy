ab668827717f4a259875e20fed13fdde874143d4
Ivo Slanina <ivo.slanina@gmail.com>
2020-12-08 10:09:40 +0100

    Added tests

--- src/test/groovy/cz/buben/sre/model/convert/PathConverterSpecification.groovy
+++ src/test/groovy/cz/buben/sre/model/convert/PathConverterSpecification.groovy
@@ -1,13 +1,35 @@
 package cz.buben.sre.model.convert
 
-import spock.lang.Ignore
 import spock.lang.Specification
+
+import java.nio.file.Paths
 
 class PathConverterSpecification extends Specification {
 
-    @Ignore
-    def "implement tests"() {
+    PathConverter converter = new PathConverter()
+
+    def "convert string to path"() {
         expect:
-        false
+        converter.convertToEntityAttribute('a/b/c/d') == Paths.get('a/b/c/d')
+    }
+
+    def "convert path to string"() {
+        expect:
+        converter.convertToDatabaseColumn(Paths.get('a/b/c/d')) == 'a/b/c/d'
+    }
+
+    def "null string is converted to null path"() {
+        expect:
+        converter.convertToEntityAttribute(null) == null
+    }
+
+    def "empty string is converted to null path"() {
+        expect:
+        converter.convertToEntityAttribute('') == null
+    }
+
+    def "null path is converted to null string"() {
+        expect:
+        converter.convertToDatabaseColumn(null) == null
     }
 }
